0ac79d07152c484de0741f908806e304acc168cb
te-ta <soul911@bk.ru>
2020-05-07 14:51:50 +0000

    contactform update

--- scripts/callback.js
+++ scripts/callback.js
@@ -3,21 +3,26 @@ $(function(){
 		$('.form__container').fadeOut(400);
 		$('html, body').removeClass('dont-move');
     $(document).unbind('touchmove');
-	}
+    $('.callback__email').fadeOut(400);
+	};
     // callback form 
-    $('.callback').click(function(){
-        $('.form__container').fadeIn(400).css("display", "flex");
-        $('html, body').addClass('dont-move');
-        $(document).bind('touchmove', false);
-    });
+  $('.callback').click(function(){
+  	let formTitle = $(this).attr("aria-label");
+    $('.form__container').fadeIn(400).css("display", "flex");
+    $('html, body').addClass('dont-move');
+    $(document).bind('touchmove', false);
+    $('.callback__title').text(formTitle);
+  });
 
-    $('.form__container').click(function(event){
-        if(event.target == this){
-            $(this).fadeOut(400);
-            $('html, body').removeClass('dont-move');
-            $(document).unbind('touchmove');
-        };
-    });
+  $('.button_red').click(function(){
+    $('.callback__email').show();
+  });
+
+  $('.form__container').click(function(event){
+    if(event.target == this){
+      closePopup();
+    };
+  });
 
 	$('.form__container_close').click(function(){
 		closePopup();
@@ -25,7 +30,8 @@ $(function(){
 
 
 	// validation 
-	$('input[type="tel"]').inputmask({"mask": "+7 (999) 999-9999"}); 
+	$('input[type="tel"]').inputmask({"mask": "+7 (999) 999-9999"});
+	$('#email').inputmask("email");
 	
 	$('form').each(function () {
 		$(this).validate({
@@ -40,10 +46,14 @@ $(function(){
 				Имя: {
 					required: true,
 					minlength: 3,
+				},
+				email: {
+					required: true,
+					email: true,
 				}
 			},
 			submitHandler(form) {
-				let doneMsg = $('<div class="done-msg"><div class="callback__done">Заявка получена.</div><p>Спасибо! Я свяжусь с вами в ближайшее время.</p></div>');
+				let doneMsg = $('<div class="done-msg">Заявка отправлена!</div>');
 				let th = $(form);
 				$.ajax({
 					type: 'POST',
@@ -53,7 +63,7 @@ $(function(){
 				}).done(() => {
 					th.trigger('reset');
 					th.replaceWith(doneMsg);
-					setTimeout(() => closePopup(),2500);
+					setTimeout(() => closePopup(),3500);
 				});
 				return false;
 			},
